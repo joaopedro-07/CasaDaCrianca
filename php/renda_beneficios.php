8b24ab754eff0b64acee143cb685979fe2dfe348
Victor Koba <victorkoba08@gmail.com>
2026-05-04 11:49:04 -0300

    header.php, include nos demais arquivos

--- php/renda_beneficios.php
+++ php/renda_beneficios.php
@@ -33,31 +33,8 @@ if ($stmt) {
 <body class="body-padrao">
     <?php include 'sidebar.php'; ?>
     <div class="container-geral">
-        <header class="header-padrao">
-            <div class="div-search">
-                <svg class="icon-search" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
-                <input class="input-search" type="text" placeholder="Pesquisar criança...">
-            </div>
-            <div class="lado-direito-header">
-                <div class="notificacao-container">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"/><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"/></svg>
-                    <span class="ponto-notificacao"></span>
-                </div>
-
-                <div class="linha-vertical-header"></div>
-
-                <div class="perfil-header">
-                    <div class="avatar-circulo">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#FCC404" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
-                    </div>
-                    <div class="info-admin">
-                        <span class="nome-admin">
-                            <?php echo $linha ? $linha['nome'] : 'Admin'; ?>
-                        </span>
-                        <span class="cargo-admin">Gerenciador</span>
-                    </div>
-                </div>
-            </div>
+        <header>
+            <?php include 'header.php'; ?>  
         </header>
     </div>
 </body>
