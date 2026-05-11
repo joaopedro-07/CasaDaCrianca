a272b984ab0a8b8968272aebab7b837e9032ce59
Victor Koba <victorkoba08@gmail.com>
2026-05-11 12:50:44 -0300

    subindo conexao correta

--- conexao.php
+++ conexao.php
@@ -1,12 +1,14 @@
 <?php
-    $dbhost="localhost";
-    $dbuser="root";
-    $dbpassword="";
-    $dbname="db_casadacrianca";
 
-    $conn = mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
-    
-    if(!$conn){
-        die("Falhou a conexão". mysqli_connect_error());
-    }
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$db   = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'];
+
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
+
+if (!$conn) {
+    die("Erro na conexão: " . mysqli_connect_error());
+}
 ?>
