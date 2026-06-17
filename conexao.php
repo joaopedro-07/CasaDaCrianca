f489ab838ea2025766329ee3fb8a42ad69529bed
victorkoba <victorkoba08@gmail.com>
2026-06-17 16:03:59 +0000

    .env-example

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = $_ENV['DB_HOST'] ?? 'localhost';
-$user = $_ENV['DB_USER'] ?? 'root';
-$pass = $_ENV['DB_PASS'] ?? '';
-$db   = $_ENV['DB_NAME'] ?? 'db_casadacrianca';
+$host = $_ENV['DB_HOST'] ?? '127.0.0.1';
+$user = $_ENV['DB_USERNAME'] ?? 'usuario';
+$pass = $_ENV['DB_PASSWORD'] ?? 'senha123';
+$db   = $_ENV['DB_DATABASE'] ?? 'db_casadacrianca';
 $port = $_ENV['DB_PORT'] ?? '3306';
 
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
